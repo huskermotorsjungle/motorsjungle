1b1c3cd31ac0858dd1cdcc3d3a698b020337253f
Andy Otte <huskermotorsjungle@gmail.com>
2025-04-18 13:57:35 -0500

    Add show action to listings routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "/thecarjungle", to: "car_jungle#show"
   post "/leads", to: "leads#create", as: "leads"
 
-  resources :listings, only: [:new, :create, :index, :edit, :update, :destroy]
+  resources :listings, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 end
 
   
