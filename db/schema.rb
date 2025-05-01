454e4cb4c2ed2b5bb110dce23572a98f0e65a04f
Andy Otte <huskermotorsjungle@gmail.com>
2025-05-01 11:31:57 -0500

    Update schema.rb after adding location to listings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_18_183356) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_01_161202) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_18_183356) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
