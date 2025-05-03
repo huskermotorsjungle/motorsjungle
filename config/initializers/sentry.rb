86719263ff376d44d93e494c8e1fee569ef053ab
Andy Otte <huskermotorsjungle@gmail.com>
2025-05-03 15:09:41 -0500

    Guard Sentry init behind presence of SENTRY_DSN

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
+# Only initialize Sentry if someone has set a DSN
+dsn = ENV["SENTRY_DSN"]
+return unless dsn
+
 Sentry.init do |config|
   # pull your DSN from the environment
-  config.dsn = ENV.fetch("SENTRY_DSN")
+  config.dsn = dsn
 
   # enable both Rails and HTTP breadcrumbs
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
@@ -10,6 +14,6 @@ Sentry.init do |config|
   # include PII (e.g. user IP, headers) in events
   config.send_default_pii = true
 
-  # adjust sampling if you want performance tracing
-  # config.traces_sample_rate = 1.0
+  # performance tracing: use SENTRY_TRACES_SAMPLE_RATE (0.0 if unset)
+  config.traces_sample_rate = ENV.fetch("SENTRY_TRACES_SAMPLE_RATE", 0.0).to_f
 end
