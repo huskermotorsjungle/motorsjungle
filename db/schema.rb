fefbb6099c1ec9a6148ac9558b3b07448f9c950d
Andy Otte <huskermotorsjungle@gmail.com>
2025-05-05 13:55:15 -0500

    chore: remove Flipper schema and gems

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_01_161202) do
-
-
+ActiveRecord::Schema[7.1].define(version: 2025_05_03_182921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
