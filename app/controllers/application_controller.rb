87b895d14af97f39bca2c2b0a35bfd26bfa933f3
Andy Otte <huskermotorsjungle@gmail.com>
2025-05-05 19:30:36 -0500

    feat: permit account_type in Devise sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # Allow extra Devise params (like account_type) on sign up
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # Permit :account_type when a user signs up
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:account_type])
+  end
 end
