9c7a5009f79c77adbf4538f523001b2ada29b154
Andy Otte <huskermotorsjungle@gmail.com>
2025-05-09 16:52:15 -0500

    fix: logout link now works via JS or GET fallback

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,5 @@
+import "@hotwired/turbo-rails"
+import Rails from "@rails/ujs"
+Rails.start()
+
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root "home#welcome"
+  # Update Devise routes to accept GET and DELETE for sign_out
+  devise_for :users, sign_out_via: [:get, :delete]
 
+  root "home#welcome"
   get  "/thecarjungle", to: "car_jungle#show"
   post "/leads",         to: "leads#create", as: "leads"
-
   resources :listings, only: %i[new create index show edit update destroy]
-
-  # Only allow HTTP-Basic admins to hit /flipper
 end
+
+
+
   
 
